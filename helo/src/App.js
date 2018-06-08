683ad04b2220adc727ba398abc29077a072408a5
Mac Read <james.macgregor.read@gmail.com>
2018-06-08 10:00:54 -0600

    react setup complete

---
+++ helo/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Nav from './component/Nav/Nav';
+import Auth from './component/Auth/Auth';
+import Dashboard from './component/Dashboard/Dashboard';
+import Form from './component/Form/Form';
+import Post from './component/Post/Post';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <Auth />
+        <Dashboard />
+        <Form />
+        <Post />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ helo/src/component/Nav/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Nac(props) {
+  return (
+    <div className="Nav">
+       Nav
+    </div> 
+  )
+}
